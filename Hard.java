c1757351e22a212f83eb0cfc760b7c879cb9fd01
ihavespoons <bgittins22@gmail.com>
2013-10-17 16:18:42 +1000

    Difficulty selection almost finalised

--- Hard.java
+++ Hard.java
@@ -5,14 +5,30 @@ import javax.imageio.ImageIO;
 import java.io.*;
 import java.awt.image.*;
 import java.awt.event.*;
-
+import javax.swing.filechooser.FileView;
 
 public class Hard extends JFrame
 {
   private String user;
   private String difficulty = "h";
-  public Hard(String user)
+  private String location = "games/hard/";
+
+  private File loadFile;
+ 
+ final JFileChooser fc = new JFileChooser(location);
+ 
+ public Hard(String user)
  {
    this.user = user;
+    fc.setFileView(new FileView() {
+    @Override
+    public Boolean isTraversable(File f) {
+         return location.equals(f);
+    }
+ });
+ 
+ int returnVal = fc.showOpenDialog(this);
+ 
+ loadFile = fc.getSelectedFile();
  }
 }
--- Medium.java
+++ Medium.java
@@ -5,14 +5,28 @@ import javax.imageio.ImageIO;
 import java.io.*;
 import java.awt.image.*;
 import java.awt.event.*;
+import javax.swing.filechooser.FileView;
 
 
 public class Medium extends JFrame
 {
  private String user;
  private String difficulty = "m";
+ private String location = "games/medium/";
+
+ private File loadFile;
+ 
+ final JFileChooser fc = new JFileChooser(location);
  public Medium(String user)
 {
   this.user = user;
+  fc.setFileView(new FileView() {
+    @Override
+    public Boolean isTraversable(File f) {
+         return location.equals(f);
+    }
+ });
+ int returnVal = fc.showOpenDialog(this);
+ loadFile = fc.getSelectedFile();
 }
 }
