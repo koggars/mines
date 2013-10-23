9132bb2ab748f63b05c5b9eabf55ab80d21eff4c
ihavespoons <bgittins22@gmail.com>
2013-10-23 23:44:08 +1000

    update relative positioning for windows

--- gui/MinesMain.java
+++ gui/MinesMain.java
@@ -12,251 +12,252 @@ import java.awt.event.*;
 // Main Function for the Mines App
 
 public class MinesMain extends JFrame {
-	private GameSelect gameSelect;
+ private GameSelect gameSelect;
 
-	private JLabel statusBar;
-	private JLabel title;
-	private JPanel placeHolder;
+ private JLabel statusBar;
+ private JLabel title;
+ private JPanel placeHolder;
 
-	private JMenuBar menuPlace;
+ private JMenuBar menuPlace;
 
-	private JMenu menu;
-	private JMenu file;
-	private JMenu edit;
-	private JMenu actions;
-	private JMenu help;
+ private JMenu menu;
+ private JMenu file;
+ private JMenu edit;
+ private JMenu actions;
+ private JMenu help;
 
-	private JMenuItem mainMenu;
-	private JMenuItem gameMenu;
-	private JMenuItem saveGame;
-	private JMenuItem restartGame;
-	private JMenuItem quitGame;
-	private JMenuItem undoLast;
-	private JMenuItem redoLast;
-	private JMenuItem solveGame;
-	private JMenuItem highScores;
-	private JMenuItem helpPopup;
+ private JMenuItem mainMenu;
+ private JMenuItem gameMenu;
+ private JMenuItem saveGame;
+ private JMenuItem restartGame;
+ private JMenuItem quitGame;
+ private JMenuItem undoLast;
+ private JMenuItem redoLast;
+ private JMenuItem solveGame;
+ private JMenuItem highScores;
+ private JMenuItem helpPopup;
 
-	private JMenuItem aboutPopup;
+ private JMenuItem aboutPopup;
 
-	private BoardFrame gameBoard;
-	private MineSaveFileIO mineSFIO;
+ private BoardFrame gameBoard;
+ private MineSaveFileIO mineSFIO;
 
-	private String user;
+ private String user;
 
-	private MineGameFile currentGame;
+ private MineGameFile currentGame;
 
-	public MinesMain(GameSelect gameSelect, String userName, MineGameFile gameFile) {
-		this.gameSelect = gameSelect;
-		currentGame = gameFile;
-		user = userName;
+ public MinesMain(GameSelect gameSelect, String userName, MineGameFile gameFile) {
+  this.gameSelect = gameSelect;
+  currentGame = gameFile;
+  user = userName;
 
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setTitle("Minesweeper In Game");
-	}
+  setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  setTitle("Minesweeper In Game");
+ }
 
-	public MinesMain(GameSelect gameSelect, String userName, MineGameFile gameFile, MineSaveFile gameSave) {
-		this.gameSelect = gameSelect;
-		currentGame = gameFile;
-		user = userName;
+ public MinesMain(GameSelect gameSelect, String userName, MineGameFile gameFile, MineSaveFile gameSave) {
+  this.gameSelect = gameSelect;
+  currentGame = gameFile;
+  user = userName;
 
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setTitle("Minesweeper In Game");
+  setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  setTitle("Minesweeper In Game");
 
-		user = userName;
+  user = userName;
 
-		loadGame(gameSave);
-	}
+  loadGame(gameSave);
+ }
 
-	public void loadGame(MineSaveFile gameSave) {
-		title = new JLabel("Game: " + currentGame.getSeed() + " " + currentGame.getDifficulty() + " - " + user);
+ public void loadGame(MineSaveFile gameSave) {
+  title = new JLabel("Game: " + currentGame.getSeed() + " " + currentGame.getDifficulty() + " - " + user);
 
-		mineSFIO = new MineSaveFileIO(user);
+  mineSFIO = new MineSaveFileIO(user);
 
-		Board currentBoard = new Board(currentGame.getDifficulty(), currentGame.getSeedLong(), gameSave.getGameBoard());
+  Board currentBoard = new Board(currentGame.getDifficulty(), currentGame.getSeedLong(), gameSave.getGameBoard());
 
-		statusBar = new JLabel("");
+  statusBar = new JLabel("");
 
-		gameBoard = new BoardFrame(statusBar, currentBoard);
-		gameBoard.newGame();
+  gameBoard = new BoardFrame(statusBar, currentBoard);
+  gameBoard.newGame();
 
-		int[] rc = currentBoard.getRowsColsMines();
-		int width = rc[1] * 15;
-		int height = rc[0] * 15;
-		gameBoard.setPreferredSize(new Dimension(width, height));
+  int[] rc = currentBoard.getRowsColsMines();
+  int width = rc[1] * 15;
+  int height = rc[0] * 15;
+  gameBoard.setPreferredSize(new Dimension(width, height));
 
-		initComponents();
-		setEventHandlers();
-	}
+  initComponents();
+  setEventHandlers();
+ }
 
-	public void newGame() {
-		title = new JLabel("Game: " + currentGame.getSeed() + " " + currentGame.getDifficulty() + " - " + user);
+ public void newGame() {
+  title = new JLabel("Game: " + currentGame.getSeed() + " " + currentGame.getDifficulty() + " - " + user);
 
-		mineSFIO = new MineSaveFileIO(user);
+  mineSFIO = new MineSaveFileIO(user);
 
-		Board currentBoard = new Board(currentGame.getDifficulty(), currentGame.getSeedLong(), currentGame.getMineData());
+  Board currentBoard = new Board(currentGame.getDifficulty(), currentGame.getSeedLong(), currentGame.getMineData());
 
-		statusBar = new JLabel("");
+  statusBar = new JLabel("");
 
-		gameBoard = new BoardFrame(statusBar, currentBoard);
-		gameBoard.newGame();
+  gameBoard = new BoardFrame(statusBar, currentBoard);
+  gameBoard.newGame();
 
-		int[] rc = currentBoard.getRowsColsMines();
-		int width = rc[1] * 15;
-		int height = rc[0] * 15;
-		gameBoard.setPreferredSize(new Dimension(width, height));
+  int[] rc = currentBoard.getRowsColsMines();
+  int width = rc[1] * 15;
+  int height = rc[0] * 15;
+  gameBoard.setPreferredSize(new Dimension(width, height));
 
-		initComponents();
-		setEventHandlers();
-	}
+  initComponents();
+  setEventHandlers();
+ }
 
-	public void initComponents() {
+ public void initComponents() {
 
-		menuPlace = new JMenuBar();
+  menuPlace = new JMenuBar();
 
-		placeHolder = new JPanel();
+  placeHolder = new JPanel();
 
-		menu = new JMenu("Menu");
-		file = new JMenu("File");
-		edit = new JMenu("Edit");
-		actions = new JMenu("Actions");
-		help = new JMenu("Help");
+  menu = new JMenu("Menu");
+  file = new JMenu("File");
+  edit = new JMenu("Edit");
+  actions = new JMenu("Actions");
+  help = new JMenu("Help");
 
-		mainMenu = new JMenuItem("Main Menu");
-		gameMenu = new JMenuItem("Game Menu");
-		saveGame = new JMenuItem("Save Game");
-		restartGame = new JMenuItem("Restart Game");
-		quitGame = new JMenuItem("Quit");
-		undoLast = new JMenuItem("Undo");
-		redoLast = new JMenuItem("Redo");
-		solveGame = new JMenuItem("Solve Current Game");
-		highScores = new JMenuItem("Scores & Stats");
-		helpPopup = new JMenuItem("Help");
-		aboutPopup = new JMenuItem("About Minesweeper");
-
-
-		// Adding of components
-		add(statusBar, BorderLayout.SOUTH);
-		add(menuPlace, BorderLayout.NORTH);
-		add(placeHolder, BorderLayout.CENTER);
+  mainMenu = new JMenuItem("Main Menu");
+  gameMenu = new JMenuItem("Game Menu");
+  saveGame = new JMenuItem("Save Game");
+  restartGame = new JMenuItem("Restart Game");
+  quitGame = new JMenuItem("Quit");
+  undoLast = new JMenuItem("Undo");
+  redoLast = new JMenuItem("Redo");
+  solveGame = new JMenuItem("Solve Current Game");
+  highScores = new JMenuItem("Scores & Stats");
+  helpPopup = new JMenuItem("Help");
+  aboutPopup = new JMenuItem("About Minesweeper");
 
 
-		menuPlace.add(menu);
-		menu.add(mainMenu);
-		menu.add(gameMenu);
-		menu.add(quitGame);
+  // Adding of components
+  add(statusBar, BorderLayout.SOUTH);
+  add(menuPlace, BorderLayout.NORTH);
+  add(placeHolder, BorderLayout.CENTER);
 
-		menuPlace.add(file);
-		file.add(restartGame);
-		file.add(saveGame);
 
-		menuPlace.add(edit);
-		edit.add(undoLast);
-		edit.add(redoLast);
+  menuPlace.add(menu);
+  menu.add(mainMenu);
+  menu.add(gameMenu);
+  menu.add(quitGame);
 
-		gameBoard.setMenus(undoLast, redoLast);
+  menuPlace.add(file);
+  file.add(restartGame);
+  file.add(saveGame);
 
-		redoLast.setEnabled(false);
-		undoLast.setEnabled(false);
+  menuPlace.add(edit);
+  edit.add(undoLast);
+  edit.add(redoLast);
 
-		menuPlace.add(actions);
-		actions.add(solveGame);
-		actions.add(highScores);
+  gameBoard.setMenus(undoLast, redoLast);
 
-		menuPlace.add(help);
-		help.add(helpPopup);
-		help.add(aboutPopup);
+  redoLast.setEnabled(false);
+  undoLast.setEnabled(false);
 
-		placeHolder.setLayout(new BorderLayout());
-		placeHolder.add(title, BorderLayout.NORTH);
-		placeHolder.add(gameBoard, BorderLayout.CENTER);
-		placeHolder.repaint();
-		setLocationRelativeTo(null);
-		setResizable(false);
+  menuPlace.add(actions);
+  actions.add(solveGame);
+  actions.add(highScores);
 
-		this.repaint();
-		this.pack();
-		setVisible(true);
-	}
+  menuPlace.add(help);
+  help.add(helpPopup);
+  help.add(aboutPopup);
 
-	public void setEventHandlers() {
-		saveGame.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent f) {
-				if (gameBoard.isInGame()) {
-					MineSaveFile saveFile = new MineSaveFile(currentGame, user, gameBoard.getField());
-					if (mineSFIO.checkMineFile(saveFile) && confirmDialog("Save File Already exists do you wish to overwrite? \n All Saved Progress will be lost"))
-						mineSFIO.createMineFile(saveFile);
-					else
-						mineSFIO.createMineFile(saveFile);
+  placeHolder.setLayout(new BorderLayout());
+  placeHolder.add(title, BorderLayout.NORTH);
+  placeHolder.add(gameBoard, BorderLayout.CENTER);
+  placeHolder.repaint();
+  setLocationRelativeTo(null);
+  setResizable(false);
 
-					gameSelect.setListData();
-					gameSelect.setVisible(true);
-					dispose();
-				} else {
-					JOptionPane.showMessageDialog(null, "Error: Unable to save game \n Cannot Save a game that you have lost!");
-				}
-			}
-		});
+  this.repaint();
+  this.pack();
+  this.setLocationRelativeTo(null);
+  setVisible(true);
+ }
 
-		restartGame.addActionListener(new ActionListener() {
+ public void setEventHandlers() {
+  saveGame.addActionListener(new ActionListener() {
+   public void actionPerformed(ActionEvent f) {
+    if (gameBoard.isInGame()) {
+     MineSaveFile saveFile = new MineSaveFile(currentGame, user, gameBoard.getField());
+     if (mineSFIO.checkMineFile(saveFile) && confirmDialog("Save File Already exists do you wish to overwrite? \n All Saved Progress will be lost"))
+      mineSFIO.createMineFile(saveFile);
+     else
+      mineSFIO.createMineFile(saveFile);
 
-			public void actionPerformed(ActionEvent f) {
-				if (confirmDialog("Do You Wish to Restart Game?")) {
-					gameSelect.restartGame();
-				}
-			}
-		});
-		undoLast.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent actionEvent) {
-				gameBoard.undoMove();
-			}
-		});
-		redoLast.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent actionEvent) {
-				gameBoard.redoMove();
-			}
-		});
-		solveGame.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent f) {
-				if (confirmDialog("Do you Wish to Solve?  \n All Progress will be lost!")) {
-					gameBoard.solveGame();
-				}
-			}
-		});
+     gameSelect.setListData();
+     gameSelect.setVisible(true);
+     dispose();
+    } else {
+     JOptionPane.showMessageDialog(null, "Error: Unable to save game \n Cannot Save a game that you have lost!");
+    }
+   }
+  });
 
-		mainMenu.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent f) {
-				if (confirmDialog("Do You wish to go back to the Main Menu? \n All Progress will be lost!")) {
-					gameSelect.showSplash();
-					dispose();
-				}
-			}
-		});
-		gameMenu.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent f) {
-				if (confirmDialog("Do You wish to go back to the Game Select Menu? \n All Progress will be lost!")) {
-					gameSelect.setVisible(true);
-					dispose();
-				}
-			}
-		});
-		quitGame.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent f) {
-				if (confirmDialog("So You wish to quit? \n All Progress will be lost!")) {
-					System.exit(0);
-				}
-			}
-		});
-	}
+  restartGame.addActionListener(new ActionListener() {
 
-	public MineGameFile getGameFile() {
-		return currentGame;
-	}
+   public void actionPerformed(ActionEvent f) {
+    if (confirmDialog("Do You Wish to Restart Game?")) {
+     gameSelect.restartGame();
+    }
+   }
+  });
+  undoLast.addActionListener(new ActionListener() {
+   @Override
+   public void actionPerformed(ActionEvent actionEvent) {
+    gameBoard.undoMove();
+   }
+  });
+  redoLast.addActionListener(new ActionListener() {
+   @Override
+   public void actionPerformed(ActionEvent actionEvent) {
+    gameBoard.redoMove();
+   }
+  });
+  solveGame.addActionListener(new ActionListener() {
+   public void actionPerformed(ActionEvent f) {
+    if (confirmDialog("Do you Wish to Solve?  \n All Progress will be lost!")) {
+     gameBoard.solveGame();
+    }
+   }
+  });
 
-	public boolean confirmDialog(String message) {
-		int out = JOptionPane.showConfirmDialog(this, message, "Confirm", JOptionPane.WARNING_MESSAGE, JOptionPane.YES_NO_OPTION);
-		return out == 0;
-	}
+  mainMenu.addActionListener(new ActionListener() {
+   public void actionPerformed(ActionEvent f) {
+    if (confirmDialog("Do You wish to go back to the Main Menu? \n All Progress will be lost!")) {
+     gameSelect.showSplash();
+     dispose();
+    }
+   }
+  });
+  gameMenu.addActionListener(new ActionListener() {
+   public void actionPerformed(ActionEvent f) {
+    if (confirmDialog("Do You wish to go back to the Game Select Menu? \n All Progress will be lost!")) {
+     gameSelect.setVisible(true);
+     dispose();
+    }
+   }
+  });
+  quitGame.addActionListener(new ActionListener() {
+   public void actionPerformed(ActionEvent f) {
+    if (confirmDialog("So You wish to quit? \n All Progress will be lost!")) {
+     System.exit(0);
+    }
+   }
+  });
+ }
+
+ public MineGameFile getGameFile() {
+  return currentGame;
+ }
+
+ public boolean confirmDialog(String message) {
+  int out = JOptionPane.showConfirmDialog(this, message, "Confirm", JOptionPane.WARNING_MESSAGE, JOptionPane.YES_NO_OPTION);
+  return out == 0;
+ }
 }
